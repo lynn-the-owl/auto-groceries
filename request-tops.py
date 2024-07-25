f67dcb423d2cdfb436b593b881ead460bd25352a
lynn-the-owl <lha4school@gmail.com>
2024-07-26 03:59:44 +0700

    TOPS request handler
    Selenium code for requesting a product page from TOPS.

--- request-tops.py
+++ request-tops.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-# Set up the WebDriver (this example uses Chrome)
-driver = webdriver.Chrome()
+product_id = "8850144226574"
 
-# Open Google
-driver.get("https://www.tops.co.th/en/search/8850144226574")
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-infobars")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# Open the website
+driver.get("https://www.tops.co.th/en/search/" + product_id)
 
 # Wait for the results to load
-time.sleep(3) 
+time.sleep(0.5) 
 
 # Click on the first link in the search results
 first_result = driver.find_element(By.CLASS_NAME, 'product-item-inner-wrap')
@@ -24,13 +33,26 @@ driver.quit
 
 import requests
 
-productCode = "8850144226574"
-
 url = getSeleniumUrl
 print(url)
 
-rawHTML = requests.get(url)
+rawHTML = requests.get(url).text
 
-file = open("response.html", "w")
-file.write(rawHTML.text)
-file.close
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(rawHTML, 'html.parser')
+
+pulledData = soup.find('script', id='meta-schema').get_text()
+print(pulledData)
+
+import json
+from types import SimpleNamespace
+
+x = json.loads(pulledData, object_hook=lambda d: SimpleNamespace(**d))
+productName = x.name
+productPrice = x.offers.price
+
+print(productName)
+print(productPrice)
+
